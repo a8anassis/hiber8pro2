a87bc926d426be1317569d2b442a99d5b861e0c4
a8anassis <a8anassis@gmail.com>
2025-07-07 21:01:41 +0300

    fix: add pu

--- src/main/java/gr/aueb/cf/schoolapp/App.java
+++ src/main/java/gr/aueb/cf/schoolapp/App.java
@@ -1,8 +1,17 @@
 package gr.aueb.cf.schoolapp;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
+
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school8PU");
+    private static EntityManager em = emf.createEntityManager();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
     }
 }
