dcd9063cf470bd15bd53aa45be754df7cc8de39a
a8anassis <a8anassis@gmail.com>
2025-07-08 19:24:56 +0300

    feat: add JPQL and criteria api for queries

--- src/main/java/gr/aueb/cf/schoolapp/App.java
+++ src/main/java/gr/aueb/cf/schoolapp/App.java
@@ -1,12 +1,16 @@
 package gr.aueb.cf.schoolapp;
 
 
+import gr.aueb.cf.schoolapp.model.Course;
+import gr.aueb.cf.schoolapp.model.Region;
 import gr.aueb.cf.schoolapp.model.Teacher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 
+import javax.xml.transform.stream.StreamResult;
 import java.util.List;
 
 public class App {
@@ -17,11 +21,74 @@ public class App {
     public static void main(String[] args) {
 
         // Select active teachers - JPQL
-        TypedQuery<Teacher> query = em.createQuery("SELECT t FROM Teacher t WHERE active = true", Teacher.class);
-        List<Teacher> teachers = query.getResultList();
-        teachers.forEach(System.out::println);
+//        TypedQuery<Teacher> query = em.createQuery("SELECT t FROM Teacher t WHERE active = true", Teacher.class);
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
 
-        // C
+        // Criteria API - Select active teachers
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> query1 = cb.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query1.from(Teacher.class);
+//        query1.select(teacher)
+//                .where(
+//                    cb.isTrue(teacher.get("active"))
+//                );
+//        List<Teacher> teachers = em.createQuery(query1).getResultList();
+//        teachers.forEach(System.out::println);
+
+        // Select active teachers from "Αθήνα" - JPQL
+//        String sql = "SELECT t FROM Teacher t WHERE active = true AND t.region.title = :regionTitle";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("regionTitle", "Αθήνα");
+//        List<Teacher> activeTeachersInRegion = query.getResultList();
+//        activeTeachersInRegion.forEach(System.out::println);
+
+
+        // Select active teachers from "Αθήνα" - Criteria API
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//
+//        CriteriaQuery<Teacher> query = criteriaBuilder.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query.from(Teacher.class);
+//        Join<Teacher, Region> region =  teacher.join("region");
+//        ParameterExpression<String> regionTitle = criteriaBuilder.parameter((String.class));
+//
+//        query.select(teacher)
+//                .where(
+//                        criteriaBuilder.and(
+//                                criteriaBuilder.isTrue(teacher.get("active")),
+//                                criteriaBuilder.equal(region.get("title"), regionTitle)
+//                        )
+//                );
+//        List<Teacher> activeTeachersInRegion = em.createQuery(query)
+//                .setParameter(regionTitle, "Αθήνα")
+//                .getResultList();
+//        activeTeachersInRegion.forEach(System.out::println);
+
+        // Select courses with comments containing with 'Coding' - JPQL
+//        String sql = "SELECT c FROM Course c WHERE c.comments LIKE :searchTerm";
+//        TypedQuery<Course> query = em.createQuery(sql, Course.class);
+//        query.setParameter("searchTerm", "%Coding%");
+//        List<Course> courses = query.getResultList();
+//        courses.forEach(System.out::println);
+
+        // Select courses with comments containing with 'Coding' - Criteria API
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//
+//        CriteriaQuery<Course> query = criteriaBuilder.createQuery(Course.class);
+//        Root<Course> course = query.from(Course.class);
+//        ParameterExpression<String> comments = criteriaBuilder.parameter((String.class));
+//
+//        query.select(course)
+//                .where(
+//                  criteriaBuilder.like(course.get("comments"), comments)
+//                );
+//
+//        List<Course> courses = em.createQuery(query).setParameter(comments, "%Coding%").getResultList();
+//        courses.forEach(System.out::println);
+
+        // Select male/female teachers born after a specific date
+        String sql = "";
+
 
     }
 }
