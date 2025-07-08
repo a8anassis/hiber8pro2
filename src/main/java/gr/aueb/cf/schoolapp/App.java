281bb6222dc8cd4e0d545ac82ef2602332921bad
a8anassis <a8anassis@gmail.com>
2025-07-08 20:08:56 +0300

    feat: add JPQL and criteria api for queries

--- src/main/java/gr/aueb/cf/schoolapp/App.java
+++ src/main/java/gr/aueb/cf/schoolapp/App.java
@@ -1,9 +1,11 @@
 package gr.aueb.cf.schoolapp;
 
 
+import gr.aueb.cf.schoolapp.enums.GenderType;
 import gr.aueb.cf.schoolapp.model.Course;
 import gr.aueb.cf.schoolapp.model.Region;
 import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.model.TeacherMoreInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,7 +13,9 @@ import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 
 import javax.xml.transform.stream.StreamResult;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 public class App {
 
@@ -86,8 +90,51 @@ public class App {
 //        List<Course> courses = em.createQuery(query).setParameter(comments, "%Coding%").getResultList();
 //        courses.forEach(System.out::println);
 
-        // Select male/female teachers born after a specific date
-        String sql = "";
+        // Select male/female teachers born after a specific date - JPQL
+//        String sql = "SELECT t FROM Teacher t JOIN t.teacherMoreInfo tmi " +
+//                "WHERE tmi.gender = :gender AND tmi.dateOfBirth > :dateOfBirth";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("gender", GenderType.MALE);
+//        query.setParameter("dateOfBirth", LocalDateTime.of(1970, 1, 1, 0, 0, 0));
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
+
+
+        // Select male/female teachers born after a specific date - Criteria API
+//        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+//
+//        CriteriaQuery<Teacher> query = criteriaBuilder.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query.from(Teacher.class);
+//        Join<Teacher, TeacherMoreInfo> teacherMoreInfo =  teacher.join("teacherMoreInfo");
+//
+//        ParameterExpression<GenderType> gender = criteriaBuilder.parameter(GenderType.class);
+//        ParameterExpression<LocalDateTime> date = criteriaBuilder.parameter(LocalDateTime.class);
+//
+//        query.select(teacher)
+//                .where(
+//                        criteriaBuilder.and(
+//                                criteriaBuilder.equal(teacherMoreInfo.get("gender"), gender),
+//                                criteriaBuilder.greaterThan(teacherMoreInfo.get("dateOfBirth"), date)
+//                        )
+//                );
+//        List<Teacher> teachers = em.createQuery(query)
+//                .setParameter(gender, GenderType.MALE)
+//                .setParameter(date, LocalDateTime.of(1970, 1, 1, 0, 0, 0))
+//                .getResultList();
+//        teachers.forEach(System.out::println);
+
+
+        // Select for every region =>  region.title and count for  inactive teachers per region
+//        String sql = "SELECT r.title, count(t) FROM Region r LEFT JOIN r.teachers t " +
+//                "WHERE t.active = false OR t.active IS NULL GROUP BY r.title";
+//        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+//        List<Object[]> inactiveTeachersCountPerRegion = query.getResultList();
+//        for (Object[] row : inactiveTeachersCountPerRegion) {
+//            for (Object item : row) {
+//                System.out.print(item + " ");
+//            }
+//            System.out.println();
+//        }
 
 
     }
